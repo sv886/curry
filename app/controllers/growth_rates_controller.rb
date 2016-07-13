018d7f4a2421ad2ef32c7399c30ba4eba33273f6
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-07-12 19:41:45 -0500

    growth rate logic in model, ctrlr, table in view, header that links to both index views

--- app/controllers/growth_rates_controller.rb
+++ app/controllers/growth_rates_controller.rb
@@ -1,5 +1,30 @@
+require 'csv'
+
 class GrowthRatesController < ApplicationController
   def index
+
+    @growth_rates = []
+
+    CSV.foreach(Rails.root + "csv/Data1.csv", headers: true) do |row|
+      #shovel csv info into array above
+
+      growth_rate = GrowthRate.new
+      growth_rate.symbol = row.to_h["symbol"]
+      growth_rate.company_name = row.to_h["company_name"]
+      growth_rate.years_dividend_growth = row.to_h["years_dividend_growth"].to_i
+      growth_rate.status = row.to_h["status"]
+      growth_rate.last_increase = row.to_h["last_increase"]
+      growth_rate.one_yr_growth = row.to_h["one_yr_growth"].to_f
+      growth_rate.three_yr_growth = row.to_h["three_yr_growth"].to_f
+      growth_rate.five_yr_growth = row.to_h["five_yr_growth"].to_f
+      growth_rate.ten_yr_growth = row.to_h["ten_yr_growth"].to_f
+
+      @growth_rates << growth_rate
+
+    end
+
+      # jwo Rails + csv vid @ 22 mins
+
   end
 
   def show
---
+++ app/models/growth_rate.rb
@@ -0,0 +1,5 @@
+class GrowthRate
+  attr_accessor :symbol, :company_name, :years_dividend_growth, :status,
+                :last_increase, :one_yr_growth, :three_yr_growth,
+                :five_yr_growth, :ten_yr_growth
+end
