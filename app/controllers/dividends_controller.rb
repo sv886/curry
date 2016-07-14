5aff2fbd517d3e7b096dffe54539da770d868704
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-07-14 12:55:53 -0500

    define fetch_dividend method in ctrlr, call in index action

--- app/controllers/dividends_controller.rb
+++ app/controllers/dividends_controller.rb
@@ -2,23 +2,25 @@ require 'csv'
 
 class DividendsController < ApplicationController
   def index
-    @dividends = []
+    @dividends = fetch_dividends
+  end
 
+  def show
+  end
+
+  def fetch_dividends
+    dividends = []
     CSV.foreach(Rails.root + "csv/DividendHistory.csv", headers: true) do |row|
       #shovel csv info into array above
-
       dividend = Dividend.new
       dividend.symbol = row.to_h["symbol"]
       dividend.date = row.to_h["date"]
       dividend.payment = row.to_h["payment"].to_f
 
-      @dividends << dividend
-
+      dividends << dividend
     end
-
-  end
-
-  def show
+    # return array
+    dividends
   end
 
 end
